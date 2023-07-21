ba2aeda6d83b217abbeb5a256d2657843e135d9b
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-07-21 20:13:33 +0530

    frontend updated with echo

--- frontend.sh
+++ frontend.sh
@@ -1,45 +1,29 @@
 #log=/tmp/roboshop.log
-#echo -e "\e[35m<<<<<<<<<<nginx installation successfully>>>>>>>>>\e[0m"
-#yum install nginx -y #&>>${log}
-#echo -e "\e[35m<<<<<<<<<<copy successfully>>>>>>>>>\e[0m"
-#cp nginx-roboshop.conf /etc/nginx/dafault.d/roboshop.conf #&>>${log}
+echo -e "\e[35m<<<<<<<<<<nginx installation successfully>>>>>>>>>\e[0m"
+yum install nginx -y #&>>${log}
+echo -e "\e[35m<<<<<<<<<<copy successfully>>>>>>>>>\e[0m"
+cp nginx-roboshop.conf /etc/nginx/dafault.d/roboshop.conf #&>>${log}
 
 
 
-#echo -e "\e[35m<<<<<<<<<<remove old files successfully>>>>>>>>>\e[0m"
-#rm -rf /usr/share/nginx/html/* #&>>${log}
+echo -e "\e[35m<<<<<<<<<<remove old files successfully>>>>>>>>>\e[0m"
+rm -rf /usr/share/nginx/html/* #&>>${log}
 
-#echo -e "\e[35m<<<<<<<<<<downloading content successfully>>>>>>>>>\e[0m"
-#curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip #&>>${log}
+echo -e "\e[35m<<<<<<<<<<downloading content successfully>>>>>>>>>\e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip #&>>${log}
 
-#echo -e "\e[35m<<<<<<<<<<change directory successfully>>>>>>>>>\e[0m"
-#cd /usr/share/nginx/html #&>>${log}
+echo -e "\e[35m<<<<<<<<<<change directory successfully>>>>>>>>>\e[0m"
+cd /usr/share/nginx/html #&>>${log}
 
-#echo -e "\e[35m<<<<<<<<<<unzip successfully>>>>>>>>>\e[0m"
-#unzip /tmp/frontend.zip #&>>${log}
-
-
-#echo -e "\e[35m<<<<<<<<<<start successfully>>>>>>>>>\e[0m"
-#systemctl enable nginx #&>>${log}
-#systemctl restart nginx #&>>${log}
+echo -e "\e[35m<<<<<<<<<<unzip successfully>>>>>>>>>\e[0m"
+unzip /tmp/frontend.zip #&>>${log}
 
 
-
-yum install nginx -y
-
-cp nginx-roboshop.conf /etc/nginx/dafault.d/roboshop.conf
-
-rm -rf /usr/share/nginx/html/*
-
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-
-cd /usr/share/nginx/html
+echo -e "\e[35m<<<<<<<<<<nginx server started successfully>>>>>>>>>\e[0m"
+systemctl enable nginx #&>>${log}
+systemctl restart nginx #&>>${log}
 
 
-unzip /tmp/frontend.zip
-
-systemctl enable nginx
-systemctl restart nginx
 
 
 
