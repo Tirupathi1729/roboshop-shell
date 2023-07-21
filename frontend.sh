ba1059545733706f9e00aaabcb9c894c4ca80879
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-07-21 09:52:36 +0530

    frontend1 updated

--- frontend.sh
+++ frontend.sh
@@ -1,12 +1,13 @@
 echo -e "\e[35m<<<<<<<<<<copy successfully>>>>>>>>>\e[0m"
 cp nginx-roboshop.conf /etc/nginx/dafault.d/roboshop.conf
 
-#yum install nginx -y
+yum install nginx -y
 
-#rm -rf /usr/share/nginx/html/*
-#curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-#cd /usr/share/nginx/html
-#unzip /tmp/frontend.zip
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
 
-#systemctl enable nginx
-#systemctl restart nginx
+systemctl enable nginx
+systemctl restart nginx
+cat /etc/nginx/dafault.d/roboshop.conf
